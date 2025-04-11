0a03db7a22bd43247baaf01da0986e9a9e8d275d
aiemsbonganay <aiembonganay24@gmail.com>
2025-04-11 09:45:21 +0800

    Initial Commit

---
+++ operation.py
@@ -0,0 +1,6 @@
+def get_even_numbers(numbers):
+    return [n for n in numbers if n % 2 == 0]
+
+
+def get_unique_items(items):
+    return list(set(items))
---
+++ test_operation.py
@@ -0,0 +1,10 @@
+from app import get_even_numbers, get_unique_items
+
+
+def test_get_even_numbers():
+    assert get_even_numbers([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+
+
+def test_get_unique_items():
+    assert sorted(get_unique_items([1, 2, 2, 3, 3, 3])) == [1, 2, 3]
+    
